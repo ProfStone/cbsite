1b618ebf1175908cc763a81fce08df6c019e3822
Derrick <derrickjstone@gmail.com>
2018-03-11 18:04:42 -0400

    added slideshow

--- index.js
+++ index.js
@@ -9,11 +9,42 @@ const app = exp();
 app.set('views','./views');
 app.set('view engine','pug');
 // set a static path
-app.use(exp.static(path.join(__dirname,'public') ));
+app.use(exp.static(path.join(__dirname, 'public')));
+app.use(exp.static(path.join(__dirname, 'images')));
 
 app.get('/', function (req,res) {
     res.render('index', {title: "Hello", h1: "Home page" });
 });
+
+app.get('/about', function (req, res) {
+    res.render('about', { title: "About", h1: "About" });
+});
+
+app.get('/contact', function (req, res) {
+    res.render('contact', { title: "Contact", h1: "Contact" });
+});
+
+app.get('/events', function (req, res) {
+    res.render('events', { title: "Events", h1: "Events" });
+});
+
+app.get('/editevents', function (req, res) {
+    res.render('editevents', { title: "Edit Events", h1: "Add/Edit Events" });
+});
+
+app.get('/subscribe', function (req, res) {
+    res.render('subscribe', { title: "Subscribe", h1: "Subscribe" });
+});
+app.post('/subscribe', function (req, res) {
+    // do somethign with the posted values here, either thank them for subscribing
+    if (true) { 
+        res.render('subscribed', { title: "Subscribe", h1: "Subscribed" });
+    // or thank them for unsubscribing
+    } else {
+        res.render('unsubscribed', { title: "Un-Subscribe", h1: "Un-Subscribe" });
+    }
+});
+
 
 app.listen(port, function () {
     console.log("Server successfully launched on port "+port);
